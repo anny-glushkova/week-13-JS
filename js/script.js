b7522c2f8a922ee62b1e6477d497ccd52ef86f9e
anny-glushkova <anna_porvatova@list.ru>
2022-01-09 16:13:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let user = prompt('Как вас зовут?');
+alert(`Привет, ${user}!`);
+
+let a = prompt('Введите первое число');
+let b = prompt('Введите второе число');
+
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
